93eff41395057d82c7f5a1ed4eedd8dd67cbf12b
Marco Castañeda <marco_pascual410@hotmail.com>
2025-05-30 13:54:42 -0600

    update

--- lib/presentation/views/auth/login.dart
+++ lib/presentation/views/auth/login.dart
@@ -156,7 +156,7 @@ class _LoginScreenState extends State<LoginScreen> {
                         ),
                       ),
                       const SizedBox(height: 25),
-                      SocialMediaAuth()
+                      const SocialMediaAuth()
                     ],
                   ),
                 ),
