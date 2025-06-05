3eb5423d6d8724d3938ae38bbec8b3bfc0e3f557
Marco Castañeda <marco_pascual410@hotmail.com>
2025-06-05 17:59:52 -0600

    icons, logo, android fixes

--- android/app/src/main/kotlin/com/kadesh/mc/MainActivity.kt
+++ android/app/src/main/kotlin/com/kadesh/mc/MainActivity.kt
@@ -2,4 +2,4 @@ package com.kadesh.mc
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity()
--- lib/presentation/views/splash/splash_screen.dart
+++ lib/presentation/views/splash/splash_screen.dart
@@ -7,6 +7,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:kadesh/data/share_prefs/prefs_usuario.dart';
 import 'package:kadesh/data/theme/style.dart';
+import 'package:kadesh/presentation/widgets/logo_widget.dart';
 
 class SplashScreen extends ConsumerStatefulWidget {
   const SplashScreen({super.key});
@@ -60,8 +61,7 @@ class SplashScreenState extends ConsumerState<SplashScreen> {
           backgroundColor: Colors.transparent,
           key: _scaffoldKey,
           body: const Center(
-            child: ImageIcon(AssetImage("assets/icons/dog-cat.png"),
-                size: 100, color: KColors.primaryColor),
+            child: LogoWidget(),
           ),
         ),
       ],
