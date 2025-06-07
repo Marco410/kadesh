87ea984f63dc1e34c05c517e1787ebe30847d0ef
Marco Castañeda <marco_pascual410@hotmail.com>
2025-06-06 18:51:44 -0600

    secrets

--- lib/data/config/firebase_config.dart
+++ lib/data/config/firebase_config.dart
@@ -1,14 +1,15 @@
 import 'dart:io';
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class DefaultFirebaseConfig {
   static FirebaseOptions get platformOptions {
     if (Platform.isIOS || Platform.isMacOS) {
       // iOS and MacOS
-      return const FirebaseOptions(
+      return FirebaseOptions(
         appId: '1:224737492413:ios:64dc0e0057b2e212d766d4',
-        apiKey: 'AIzaSyD9eAreCp_tPXPzCY7QTNkvVsfOHXJ1vY8',
+        apiKey: dotenv.env['FAPI_KEY_IOS']!,
         projectId: 'kadesh-a2d09',
         messagingSenderId: '224737492413',
         iosBundleId: 'com.kadesh.mc',
@@ -16,9 +17,9 @@ class DefaultFirebaseConfig {
       );
     } else {
       // Android
-      return const FirebaseOptions(
+      return FirebaseOptions(
         appId: '1:224737492413:android:376cfbe594dbe776d766d4',
-        apiKey: 'AIzaSyAX_plqG28-fnLV_Tt7GJoBef88fSE9pto',
+        apiKey: dotenv.env['FAPI_KEY_G']!,
         projectId: 'kadesh-a2d09',
         messagingSenderId: '224737492413',
         authDomain: 'kadesh-a2d09.firebasestorage.app',
